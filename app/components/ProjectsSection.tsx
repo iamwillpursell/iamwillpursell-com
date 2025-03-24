ed5ba215ac68a0cc43e56149a4fa676e09f47e49
Will Pursell <pursell.will@gmail.com>
2025-03-24 13:38:33 -0400

    Typo: Fixed typo in Spacey Space project description.

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -16,7 +16,7 @@ const projects = [
   {
     name: "Spacey Space",
     description:
-      "Space Space is a playful exploration of user authentication and API integration. Create an account to view a new NASA photo each day.",
+      "Spacey Space is a playful exploration of user authentication and API integration. Create an account to view a new NASA photo each day.",
     image: "/spacey-space.webp",
     github: "https://github.com/iamwillpursell/Spacey-Space",
     link: "https://spacey-space.netlify.app/login/",
